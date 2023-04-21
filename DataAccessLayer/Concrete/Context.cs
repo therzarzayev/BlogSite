8904ef7f64f849c88d7dbe6afa218409f22a1da4
therzarzayev <therzarzayev@ANGEL>
2023-04-21 11:22:44 +0400

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ANGEL; Database=TurboBlogDB; Trusted_Connection=true; Encrypt=false;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Detail1 { get; set; }
+        public string? Detail2 { get; set; }
+        public string? Image1 { get; set; }
+        public string? Image2 { get; set; }
+        public string? MapLocation { get; set; }
+        public bool Status { get; set; }
+    }
+}
